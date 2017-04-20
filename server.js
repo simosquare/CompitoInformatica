33f937e7b6e52af41d31a6b38b226843007bb224
simosquare <spiazza887@gmail.com>
2017-04-20 17:57:58 +0200

    Sito

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+app.listen(8000, () => {
+    console.log('Listening on port: %s', port);
+});
+
+app.get('/autore', function (req, res) {
+  res.send('<h1>Sito di Simone piazza</h1>')
+});
+
+app.get('/', function (req, res) {
+  res.send('<h1>Homepage</h1>')
+});
+
+app.get('*', function (req, res) {
+  res.status(404).send('Not found!')
+});
